f76d0cabb08ec8422c790d5e41d1fa67b6d71fa2
Andrius Jakas <andrius.jakas@indev.lt>
2013-03-12 09:43:01 +0200

    Added initial admin page: - admin packed with handler - template

---
+++ apenads/objects/AObject.py
@@ -0,0 +1,21 @@
+'''
+Application objects
+
+@version: 0.1
+'''
+
+class AObject(object):
+    '''
+    Base object
+    '''
+
+    data = object
+    '''
+    Data object
+    '''    
+    
+    '''
+    Constructor
+    '''
+    def __init__(self):
+        return
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import webapp2
 
+from apenads.admin.handlers import AdminLoginHandler
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('apenAds')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/admin', AdminLoginHandler),
 ], debug=True)
