238640010c63527fa594aa6faac28952b4f394ed
Andrius Jakas <andrius.jakas@indev.lt>
2013-03-19 15:53:28 +0200

    Added Dojo dijit to install and login templates

---
+++ js/main.admin.install.js
@@ -0,0 +1,9 @@
+require(["dojo/parser", 
+         "dojo/ready", 
+         "dijit/form/TextBox", 
+         "dijit/form/Button"],
+    function(parser, ready){
+        ready(function(){
+            parser.parse();
+        });
+    });
